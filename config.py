0ac012fae3ba6bac1b4e7408cc043e9052f282d6
Otajon <otajonbozorboyev571@gamil.com>
2024-03-14 14:42:48 +0500

    first commit

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 # https://home.openweathermap.org/api_keys
 
 open_weather_token = "752663b65263f83d3fc1df0dd1607750"
-tg_bot_token = "6780835036:AAEpmYsjj8eRmRd4_u6UZ3L7HxgiqNI7K50"
+tg_bot_token = "YOUR TOKEN"
